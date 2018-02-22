98a385f57c3d996adc234d854b1326da12cde43b
Ben <bphang@ucsd.edu>
2018-02-22 03:16:24 -0800

    Added Google analytics

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(function() {
+		console.log("CLICKED");
+		ga("send", "event", "like", "click");	
+	});
 }
